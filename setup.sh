384ae6f301291ead94d4245cf2633b5a7efb8b40
dwkim <dwkim@spiderkim.com>
2021-07-21 15:31:32 +0900

    chore: add install command

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+pip install torch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0
 pip install cython; pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
